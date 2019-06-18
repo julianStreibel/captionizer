f412d822b77bd87079c58ff714dadd71c8d957ae
Julian <julian.streibel@freenet.de>
2019-06-18 16:37:52 +0200

    finial init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      init: false,
+      list: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://0.0.0.0:8080/api/v1/test")
+      .then(res => res.json())
+      .then((res) => this.setState({ init: true, list: res }))
+  }
+
+  render() {
+    const { init, list } = this.state;
+    return (
+      <React.Fragment>
+        {init && list}
+        <br />
+        <RedButton>Hi</RedButton>
+        <br />
+        <br />
+        <DivButton
+          color={"orange"}
+        >Hii</DivButton>
+      </React.Fragment>
+    );
+  }
 }
+
+const RedButton = styled(Button)`
+  background-color: red !important;
+`;
+
+const DivButton = styled.div`
+  height: 20px;
+  width: 50px;
+  ${props => props.color && `background-color: ${props.color};`}
+`;
 
 export default App;
