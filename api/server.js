9139331de2ddf5f99641780b86fcd502f865a2e7
Julian <julian.streibel@freenet.de>
2019-06-19 14:13:44 +0200

    File upload, prediction und hashtags fertig

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser");
 
 const picture = require('./src/routes/picture');
 
@@ -15,23 +16,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Routes
 app.use('/api/v1/picture', picture)
-
-
-// test
-app.get('/api/v1/test', (req, res) => {
-    res.json({
-        hashtags: ["Cool", "Instagood", "pod", "Weed", "idk"],
-        captions: ["We gonna party like it`s your birthday",
-            "The more money we come across, the more problems we see",
-            "Drop it like it's hot",
-            "To live doesn't mean you're alive",
-            "I'm feelin' mysel"
-        ]
-    });
-});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
